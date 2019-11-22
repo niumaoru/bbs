016831294177f00e43c110f371de311072efdc16
NMR <niumaoru@163.com>
2019-11-22 03:58:33 +0000

    分类模型和数据

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class TestsController extends Controller
 {
     public function test(){
-        $a = false;
-        $b =2;
-        echo $a ?: $b;
+        return class_basename('App\Http\Controllers');
     }
 }
