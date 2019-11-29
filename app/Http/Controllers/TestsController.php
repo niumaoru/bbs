b1b36c8cfebbeefbdfefa00e4ec3c177b4f51abc
NMR <niumaoru@163.com>
2019-11-29 02:41:38 +0000

    用户可以创建话题

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class TestsController extends Controller
 {
     public function test(){
-        return class_basename('App\Http\Controllers');
+        $text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
+        echo strip_tags($text);
+        echo "\n";
+
+// 允许 <p> 和 <a>
+        echo strip_tags($text, '<p><a>');
     }
 }
